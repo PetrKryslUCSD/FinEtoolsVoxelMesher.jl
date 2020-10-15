25e2eacf9bc5000fc6f1dcebe283d1c7b6a7f702
Petr Krysl <pkrysl@ucsd.edu>
2020-10-15 16:46:06 -0700

    update example

--- examples/image_meshing/labrador_examples.jl
+++ examples/image_meshing/labrador_examples.jl
@@ -18,14 +18,18 @@ function labrador_1()
 	vb = VoxelBoxVolume(file.raw[:, :, :, 1], bs)
 	# vtkexport(File * ".vtk", vb)
 
+	# Set up the mesher
 	im = ImageMesher(vb, zero(eltype(vb.data)), eltype(vb.data)[100, 200])
-	im.elementsizeweightfunctions = [ElementSizeWeightFunction(20.0, vec([154.0, 158.0, 98.0]), 20.0)]
+	# We wish to refine the mesh around the molars on the left-hand side
+	setelementsizeweightfunctions(im.remesher, [ElementSizeWeightFunction(20.0, vec([154.0, 158.0, 98.0]), 20.0)])
+	
+	# Generate the initial mesh
 	println("Data read, generating initial mesh")
-	mesh!(im)
-	println("Mesh size: initial = $(size(im.t,1)) tetrahedra")
-	fens = FENodeSet(im.v)
-	fes = FESetT4(im.t)
-	setlabel!(fes, im.tmid)
+	t, v, tmid = meshdata(im)
+	println("Mesh size: initial = $(size(t,1)) tetrahedra")
+	fens = FENodeSet(v)
+	fes = FESetT4(t)
+	setlabel!(fes, tmid)
 	println("Exporting boundary mesh")
 	bfes = meshboundary(fes)
 	File = "lab-sm2-cl-mid-sep.vtk"
@@ -33,23 +37,32 @@ function labrador_1()
 	@async run(`"paraview.exe" $File`)
 	println("Done")
 
+	# Now the element size will be increased, which will reduce the number of
+	# tetrahedra
 	println("Reducing the size of the mesh")
 	for i = 1:7
-	    mesh!(im, 1.2)
-	    println("Mesh size: intermediate = $(size(im.t,1)) tetrahedra")
+	    remesh!(im, 1.2)
+	    t, v, tmid = meshdata(im)
+	    println("Mesh size: intermediate = $(size(t,1)) tetrahedra")
 	end
 	println("Done")
 
+	# The final produced mesh will be now exported for visualization
 	println("Exporting the final mesh as a surface")
-	fens = FENodeSet(im.v)
-	fes = FESetT4(im.t)
-	setlabel!(fes, im.tmid)
+	t, v, tmid = meshdata(im)
+	fens = FENodeSet(v)
+	fes = FESetT4(t)
+	setlabel!(fes, tmid)
 	bfes = meshboundary(fes)
 	File = "lab-sm2-cl-mid-sep.vtk"
 	vtkexportmesh(File, fens, bfes)
 	@async run(`"paraview.exe" $File`)
 	println("Done")
 
+	# Now save the mesh
+	FinEtools.MeshExportModule.MESH.write_MESH("lab-sm2-cl-mid-sep.mesh", fens, fes)
+
+	# If needed, save the NASTRAN file
 	# ne = NASTRANExporter("lab-sm2-cl-mid.nas")
 	# BEGIN_BULK(ne)
 	# for i = 1:count(fens)
